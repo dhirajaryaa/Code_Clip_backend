51876dee543c2b07cf55c4a56f922f30d19c0222
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2024-12-14 20:09:44 +0530

    feat: plan password to hash and access refresh token generate methods creating

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     fullName: {
@@ -37,6 +39,38 @@ const userSchema = new mongoose.Schema({
     }]
 
 }, { timestamps: true });
+
+// password to hash 
+userSchema.pre("save", async function (next) {
+    if (this.isModified) return null
+    this.password = await bcrypt.hash(this.password, 10);
+    next()
+});
+// custom methods 
+userSchema.methods.isPasswordCorrect(async function (password) {
+    return await bcrypt.compare(password, this.password);
+});
+
+userSchema.methods.generateAccessToken(async function () {
+    return await jwt.sign({
+        _id: this._id,
+        username: this.username,
+        email: this.email
+    },
+        process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+    )
+});
+userSchema.methods.generateRefreshToken(async function () {
+    return await jwt.sign({
+        _id: this._id
+    },
+        process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+    )
+});
 
 
 export const User = mongoose.model("User", userSchema);
